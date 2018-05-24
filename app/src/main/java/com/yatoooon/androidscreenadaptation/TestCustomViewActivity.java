d69df2b4214830b167ce15b7f84a57f1e4bed9f2
yatoooon <137413684@qq.com>
2018-05-24 10:47:12 +0800

    自定义控件避免重复适配

--- app/src/main/java/com/yatoooon/androidscreenadaptation/TestCustomViewActivity.java
+++ app/src/main/java/com/yatoooon/androidscreenadaptation/TestCustomViewActivity.java
@@ -3,8 +3,6 @@ package com.yatoooon.androidscreenadaptation;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.yatoooon.screenadaptation.ScreenAdapterTools;
-
 /**
  * Created by yatoooon on 2018/3/16.
  */
@@ -14,7 +12,5 @@ public class TestCustomViewActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customview);
-        ScreenAdapterTools.getInstance().loadView(getWindow().getDecorView());
-
     }
 }
